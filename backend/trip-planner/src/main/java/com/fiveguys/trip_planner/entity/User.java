aed27fc3c91a827bb9e1366be97146ea439b6ce2
Karas02000 <gta5369577@naver.com>
2026-03-30 19:34:40 +0900

    feat: entity and repository created

---
+++ backend/trip-planner/src/main/java/com/fiveguys/trip_planner/entity/User.java
@@ -0,0 +1,45 @@
+package com.fiveguys.trip_planner.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter
+@EntityListeners(AuditingEntityListener.class) // Auditing 기능을 포함시킴
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String phone;
+
+    @Column(nullable = false)
+    private String role;
+
+    @Column(nullable = false)
+    private String status;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
