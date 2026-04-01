07f2a8990914334c077f47103d524d270f47e753
남수원 <namsuwon@namsuwon-ui-MacBookPro.local>
2026-04-01 15:11:40 +0900

    feat: TripPlanController

---
+++ backend/trip-planner/src/main/java/com/fiveguys/trip_planner/controller/TripPlanController.java
@@ -0,0 +1,37 @@
+package com.fiveguys.trip_planner.controller;
+
+import com.fiveguys.trip_planner.dto.TripPlanRequestDto;
+import com.fiveguys.trip_planner.entity.User;
+import com.fiveguys.trip_planner.repository.UserRepository;
+import com.fiveguys.trip_planner.response.TripPlanResponseDto;
+import com.fiveguys.trip_planner.service.TripPlanService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api/trips")
+@RequiredArgsConstructor
+public class TripPlanController {
+    private final TripPlanService tripPlanService;
+    private final UserRepository userRepository;
+
+    @PostMapping
+    public ResponseEntity<TripPlanResponseDto> createTripPlan(
+            @RequestBody TripPlanRequestDto requestDto,
+            Principal principal // 로그인한 사용자 정보 가져오기
+            ) {
+        // 현재 로그인한 사용자의 email(또는 ID)로 User 엔티티 조회
+        String email = principal.getName();
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
+
+        TripPlanResponseDto response = tripPlanService.createTripPlan(requestDto, user);
+
+        return ResponseEntity.ok(response);
+    }
+}
